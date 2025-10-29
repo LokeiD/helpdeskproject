98ffaddc540e3c88a02515d1697159be32f7a196
Diego Fernandez <diegoferalb03@gmail.com>
2025-10-29 10:04:25 -0500

    Vistas Agregadas

--- HelpDesk/src/main/java/com/utpintegrador/helpdesk/controller/HomeController.java
+++ HelpDesk/src/main/java/com/utpintegrador/helpdesk/controller/HomeController.java
@@ -13,23 +13,51 @@ public class HomeController {
      * @return El nombre del archivo HTML en /templates
      */
     @GetMapping("/home")
-    public String showHomePage() {
+    public String mostrarHome() {
         // Esto le dice a Spring que busque el archivo:
         // /src/main/resources/templates/home.html
         return "home";
     }
 
     @GetMapping("/nuevo-ticket")
-    public String showNuevoTicketPage() {
+    public String mostrarNuevoTicket() {
         // Esto le dice a Spring/Thymeleaf que busque y muestre
         // el archivo src/main/resources/templates/nuevoticket.html
         return "nuevoticket";
     }
 
     @GetMapping("/mantenimiento/usuario")
-    public String showMntUsuarioPage() {
+    public String mostrarMntUsuario() {
         // Esto le dice a Spring que busque el archivo:
         // /src/main/resources/templates/mntusuario.html
         return "mntusuario";
+    }
+
+    @GetMapping("/mantenimiento/prioridad")
+    public String mostrarMntPrioridad() {
+        // Esto le dice a Spring que busque el archivo:
+        // /src/main/resources/templates/mntusuario.html
+        return "mntprioridad";
+    }
+
+    @GetMapping("/mantenimiento/categoria")
+    public String mostrarMntCategoria() {
+        // Esto le dice a Spring que busque el archivo:
+        // /src/main/resources/templates/mntusuario.html
+        return "mntcategoria";
+    }
+
+    @GetMapping("/mantenimiento/subcategoria")
+    public String mostrarMntSubcategoria() {
+        // Esto le dice a Spring que busque el archivo:
+        // /src/main/resources/templates/mntusuario.html
+        return "mntsubcategoria";
+    }
+
+    @GetMapping("/mantenimiento/area")
+    public String mostrarMntArea() {
+        // Esto le dice a Spring que busque el archivo:
+        // /src/main/resources/templates/mntusuario.html
+        return "mntarea";
     }
 }
